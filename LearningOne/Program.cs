bd119e89c04d4be433b3fd6e40b5780241a16a4c
asdertypo <asdertypo@126.com>
2015-01-15 22:41:15 +0800

    delegate

--- LearningOne/Program.cs
+++ LearningOne/Program.cs
@@ -8,9 +8,34 @@ namespace LearningOne
 {
     class Program
     {
+        delegate int ProcessNum(int numOne, int numTwo);
+        static int Addition(int numOne, int numTwo) {
+            int val;
+            val = numOne + numTwo;
+            return val;
+        }
+        static int Substraction(int numOne, int numTwo) {
+            int val = numOne - numTwo;
+            return val;
+        } 
         static void Main(string[] args)
         {
-
+            ProcessNum process;
+            Console.Write("Please press the first number:");
+            int one = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Good!Now please press the second number:");
+            int two = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Cool!The last step is to choose addition or substraction,press \"a\" or \"s\".");
+            string choise =Console.ReadLine();
+            if (choise.Substring(0, 1) == "a")
+            {
+                process = new ProcessNum(Addition);
+            }
+            else {
+                process = new ProcessNum(Substraction);
+            }
+            Console.WriteLine("The result is {0}", process(one, two));
+            Console.ReadKey();
         }
     }
 }
