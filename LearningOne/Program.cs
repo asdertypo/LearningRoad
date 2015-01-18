2a6a1ed19b92f594dfc05b86ec0a3ab741925468
asdertypo <asdertypo@126.com>
2015-01-18 15:37:05 +0800

    Chapter01

--- LearningOne/Program.cs
+++ LearningOne/Program.cs
@@ -6,11 +6,19 @@ using System.Threading.Tasks;
 
 namespace LearningOne
 {
+    public abstract class Mybase { }
+    internal class MyClass : Mybase { }
+    public interface IMyBaseInterface { }
+    internal interface IMyBaseinterface2 { }
+    interface IMyInterface : IMyBaseInterface, IMyBaseinterface2 { }
+    sealed class MyComplexClass : MyClass, IMyInterface { }
     class Program
     {
         static void Main(string[] args)
         {
-
+            MyComplexClass test = new MyComplexClass();
+            Console.WriteLine(typeof(MyComplexClass));
+            Console.ReadKey();
         }
     }
 }
