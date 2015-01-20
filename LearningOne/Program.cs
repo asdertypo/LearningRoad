55bba0838479d6e1f06b51bd7b03ac7612a3f186
asdertypo <asdertypo@126.com>
2015-01-20 20:42:17 +0800

    test1.i=test2.i

--- LearningOne/Program.cs
+++ LearningOne/Program.cs
@@ -6,11 +6,21 @@ using System.Threading.Tasks;
 
 namespace LearningOne
 {
+    public sealed class Myclass
+    {
+        public int i;
+    }
     class Program
     {
         static void Main(string[] args)
         {
-
+            Myclass test1 = new Myclass();
+            Myclass test2 = test1;
+            test1.i = 100;
+            test2.i = 200;
+            Console.WriteLine(test1.i);
+            Console.WriteLine(test2.i);
+            Console.ReadKey();
         }
     }
 }
