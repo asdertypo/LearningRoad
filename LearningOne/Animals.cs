82f4bc4045b229ba7ecf2584d5e22c6b27d612ee
asdertypo <asdertypo@126.com>
2015-02-03 10:26:06 +0800

    CollectionExc01(including indexer)

---
+++ LearningOne/Animals.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace LearningOne
+{
+    class Animals:CollectionBase
+    {
+        public void Add(Animal newAnimal) {
+            List.Add(newAnimal);
+        }
+        public void Remove(Animal oldAnimal) {
+            List.Remove(oldAnimal);
+        }
+        public Animals() { }
+        public Animal this[int animalIndex] {
+            get { return (Animal)List[animalIndex]; }
+            set { List[animalIndex] = value; }
+        }
+    }
+    public class Animal { 
+        protected string name;
+        public Animal(){
+            name="Defualt Name";
+        }
+        public Animal(string newName) {
+            name = newName;
+        }
+        public void feed() {
+            Console.WriteLine("{0} has been fed.", name);
+        }
+    }
+    public class Cow : Animal {
+        public Cow(string newName) : base(newName) { }
+        public void Milked(){Console.WriteLine("{0} has been milked.",name);}
+    }
+}
--- LearningOne/Program.cs
+++ LearningOne/Program.cs
@@ -10,7 +10,16 @@ namespace LearningOne
     {
         static void Main(string[] args)
         {
-
+            Animals myAnimals = new Animals();
+            for (int i = 1; i < 5; i++) {
+                myAnimals.Add(new Cow("test" + i.ToString()));
+            }
+            foreach(Animal myAnimal in myAnimals){
+                myAnimal.feed();
+            }
+            //myAnimals[0]=new Cow("Bill Gate");//使用索引符后可用
+            //myAnimals[0].feed();
+            Console.ReadKey();
         }
     }
 }
