dae96e41f66eff551eee1d3a3bd7eb72771b4b4f
asdertypo <asdertypo@126.com>
2015-02-03 09:17:49 +0800

    update

--- LearningOne/Program.cs
+++ LearningOne/Program.cs
@@ -10,7 +10,15 @@ namespace LearningOne
     {
         static void Main(string[] args)
         {
-
+            string[] names = { "smith", "apple", "snake", "park", "tick", "game", "sologn", "photo", "saw", "test" };
+            var results = names.OrderBy(n=>n).Where(c => c.StartsWith("s"));
+            //var results=from n in names
+            //            where n.StartsWith("s")
+            //            select n;
+            foreach(var result in results){
+                Console.WriteLine(result);
+            }
+            Console.ReadKey();
         }
     }
 }
